b28ab1426a178f0d8436f127d2c98a4ea421cc63
Opilane TTHK <opilane@tthk.ee>
2020-09-14 15:00:37 +0300

    Percentage Calculator

---
+++ PercentageCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace PercentageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Maximum;
+            int UserScore;
+            double Result;
+
+            Console.WriteLine("What is the maximum score?");
+            Maximum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is your score?");
+            UserScore = Int32.Parse(Console.ReadLine());
+            Result = (UserScore * 100) / Maximum;
+
+            Console.WriteLine($"You scored {Result}%.");
+
+            if(Result >= 90)
+            {
+                Console.WriteLine("You got a 5!");
+            } else if(Result >= 80)
+            {
+                Console.WriteLine("You got a 4!");
+            } else if(Result >= 61)
+            {
+                Console.WriteLine("You got a 3.");
+            } else if(Result <= 60)
+            {
+                Console.WriteLine("You failed the test");
+            }
+        }
+    }
+}
